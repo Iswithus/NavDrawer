eb3e9883cc756a96b4780173736ffebaaba43ace
Alinaffe Joseph <alinaffejoseph@gmail.com>
2023-06-03 09:51:15 +0300

    Initial commit

---
+++ app/src/main/java/com/example/navdrawer/ui/gallery/BusinessFragment.kt
@@ -0,0 +1,20 @@
+package com.example.navdrawer.ui.gallery
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.navdrawer.R
+
+class BusinessFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_business,container,false)
+    }
+
+}
